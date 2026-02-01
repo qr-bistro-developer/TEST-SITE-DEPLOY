24f872f51606413baa5e7f941078b63d345b603e
qr-bistro-developer <qr.bistro.developer@gmail.com>
2026-02-01 15:02:37 +0700

    Refactor code structure for improved readability and maintainability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,47 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import importPlugin from "eslint-plugin-import";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "react/jsx-no-undef": "error",
+      "react/prop-types": "off",
+      "react/jsx-uses-vars": "warn",
+      "arrow-parens": ["error", "always"],
+      "import/no-unresolved": "error",
+    },
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+        alias: {
+          map: [
+            ["@", "./src"],
+            ["@redux", "./src/redux"],
+            ["@components", "./src/components"],
+            ["@lib", "./src/lib"],
+            ["@utils", "./src/utils"],
+            ["@styles", "./src/styles"],
+            ["@assets", "./src/assets"],
+            ["@contexts", "./src/contexts"],
+            ["@statics", "./src/statics"],
+          ],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
+  },
 ]);
 
 export default eslintConfig;
