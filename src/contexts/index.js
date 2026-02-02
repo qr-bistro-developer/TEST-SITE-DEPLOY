58d24e76bf48a7045a3d23cd2bf8dd43227a79e8
qr-bistro-developer <qr.bistro.developer@gmail.com>
2026-02-02 20:06:17 +0700

    refactor: enhance ContextProvider to conditionally render PersistGate based on client state

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,16 +1,27 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "@/store/redux/store";
 import { StyledComponentsRegistry } from "@/contexts/StyledComponentsRegistry";
 
 export const ContextProvider = ({ children = null }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {isClient ? (
+        <PersistGate loading={null} persistor={persistor}>
+          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        </PersistGate>
+      ) : (
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      </PersistGate>
+      )}
     </Provider>
   );
 };
