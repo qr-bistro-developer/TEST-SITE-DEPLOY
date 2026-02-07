240796062c3e6b9dc1c18b363066d82815f596aa
qr-bistro-developer <qr.bistro.developer@gmail.com>
2026-02-07 11:35:53 +0700

    refactor: update import path for ContextProvider to use explicit index file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "@styles/globals.css";
 import { ibmPlexSansThai } from "@assets/fonts";
-import { ContextProvider } from "@contexts";
+import { ContextProvider } from "@contexts/index";
 
 export const metadata = {
   // metadataBase: new URL(
